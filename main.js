05f9b1c5ada65011482475a856365ab977eb08b5
MatsudaKouta <kou8722@gmail.com>
2020-10-13 23:29:49 +0900

    お問い合わせフォーム調整

--- main.js
+++ main.js
@@ -16,4 +16,35 @@
       });
     });
   });
+
+  //問い合わせフォーム
+    const patient = document.getElementById("patient");
+    const tel = document.getElementById("tel");
+    const email = document.getElementById("email");
+    const date = document.getElementById("date");
+    const time = document.getElementById("time");
+    const request = document.getElementById("request");
+    const form = document.querySelector(".contact-form");
+
+    form.addEventListener("submit", (e) => {
+      if (
+        patient.value === "" ||
+        tel.value === "" ||
+        email.value === "" ||
+        date.value === "" ||
+        time.value === "" ||
+        request.value === "" 
+      ) {
+        e.preventDefault();
+        alert("項目に入力をお願いします");
+      } else {
+        const content = document.createElement("p");
+        content.textContent =
+          "承りました。一両日中には返信させていただきます。";
+        const contactWrap = document.querySelector(".contact-wrap");
+        contactWrap.appendChild(content);
+
+        content.classList.add("submitDesign");
+      }
+    });
 }
